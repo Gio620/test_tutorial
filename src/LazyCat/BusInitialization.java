021534ec590c837b9bb14fcb948e69eec632ba5e
Giordanno Muñoz <giiohhxc@gmail.com>
2019-08-05 15:54:28 -0400

    Update BusInitialization.java
    Add comment of things to remember

--- src/LazyCat/BusInitialization.java
+++ src/LazyCat/BusInitialization.java
@@ -15,6 +15,12 @@ import LazyCat.Bus;
 
             for (int j = 0; j < this.horarios.length;j++){//recorre todos los horarios todo agregar lugares y ver el manejor de precios
 
+                /*
+                *Tengo que tener en cuenta que los de Santiago van al resto de lugares, y los de el
+                *resto de lugares solo viajan a Santiago (santiago es el hub)
+
+                */
+
             }
 
         }
