d095c5e51983dbe6211716084e57e6180332eda2
Giordanno Muñoz <giiohhxc@gmail.com>
2019-08-05 03:16:39 -0400

    first commit

---
+++ src/LazyCat/Bus.java
@@ -0,0 +1,46 @@
+package LazyCat;
+
+public class Bus {
+    private String salida= null;
+    private String destino = null;
+    private boolean [] asientos;
+    private String horario = null;
+    private int valor= 0;
+
+
+    public Bus(String salida, String destino, String horario, int valor) {
+        salida = salida;
+        destino = destino;
+        asientos = new boolean[40];
+        horario = horario;
+        valor = valor;
+    }
+
+    void checkAsientos(){
+        String ocupado;
+        for (int i = 0 ; i < asientos.length; i++){
+            ocupado = (asientos[i]) ? "X" : "O";
+            System.out.print(i+1 + ":" + ocupado);
+            if (i == 9 || i == 29){
+                System.out.print("\n");
+            }
+            if (i==19){
+                System.out.println("\n");
+            }
+        }
+    }
+
+    boolean setAsiento (int numAsiento){
+
+        boolean done = false;
+
+        if (!asientos[numAsiento-1]){
+            asientos[numAsiento-1] = true;
+            done = true;
+        }else {
+            System.out.println("Asiento N°:" + numAsiento + " Ocupado");
+        }
+
+        return done;
+    }
+}
---
+++ src/LazyCat/BusInitialization.java
@@ -0,0 +1,20 @@
+package LazyCat;
+
+import LazyCat.Bus;
+
+ class BusInitialization {
+    Object[] arrayBuses = new Object[20];
+    String [] lugares = {"Santiago" , "Copiapó", "La Serena", "Concepción", "Valdivia", "Temuco"};
+    String [] horarios = {"Mañana","Tarde"};
+    int[] precios = {10000,15000,19000,24000};
+
+    BusInitialization(){
+        for (int i = 0 ; i < this.arrayBuses.length; i++){ //recorre todos los Objetos Buses
+
+            for (int j = 0; j < this.horarios.length;j++){//recorre todos los horarios todo agregar lugares y ver el manejor de precios
+
+            }
+
+        }
+    }
+}
